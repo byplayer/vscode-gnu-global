b24b4a65e4b52ed2ed10232a8f8fe570b707af03
Goto Yukio <yukio.goto@enigmo.co.jp>
2022-06-28 13:28:57 +0900

    👍 support multi language

--- src/autoUpdateHandler.ts
+++ src/autoUpdateHandler.ts
@@ -1,36 +1,41 @@
-import * as vscode from 'vscode';
-import Global from './global/global';
-import Configuration from './configuration'
-import {BoolDefault} from './configuration'
-import Logger from './logger';
+import * as vscode from "vscode";
+import Global from "./global/global";
+import Configuration from "./configuration";
+import { BoolDefault } from "./configuration";
+import Logger from "./logger";
 
 export default class GlobalAutoUpdateHandler {
-    global: Global;
-    configuration: Configuration;
+  global: Global;
+  configuration: Configuration;
 
-    constructor(global: Global, configuration: Configuration) {
-        this.global = global;
-        this.configuration = configuration;
+  constructor(global: Global, configuration: Configuration) {
+    this.global = global;
+    this.configuration = configuration;
+  }
+
+  autoUpdateTags(docChanged: vscode.TextDocument) {
+    if (
+      docChanged.languageId !== "cpp" &&
+      docChanged.languageId !== "c" &&
+      docChanged.languageId !== "php" &&
+      docChanged.languageId !== "javascript" &&
+      docChanged.languageId !== "javascriptreact"
+    )
+      return;
+
+    try {
+      const autoUpdateMode = this.configuration.autoUpdate.get(docChanged.uri);
+
+      if (autoUpdateMode === BoolDefault.Disabled) {
+        return;
+      } else if (autoUpdateMode == BoolDefault.Default) {
+        /* Default: disable autoupdate if GTAGS size is larger than 50MB. */
+        const size = this.global.getGtagsSize(docChanged);
+        if (size >= 50 * 1024 * 1024) return;
+      }
+      this.global.updateTags(docChanged);
+    } catch (e) {
+      Logger.error("autoUpdateTags failed: " + e);
     }
-
-    autoUpdateTags(docChanged: vscode.TextDocument) {
-        if (docChanged.languageId !== "cpp" && docChanged.languageId !== "c")
-            return;
-
-        try {
-            const autoUpdateMode = this.configuration.autoUpdate.get(docChanged.uri);
-
-            if (autoUpdateMode === BoolDefault.Disabled) {
-                return;
-            } else if (autoUpdateMode == BoolDefault.Default) {
-                /* Default: disable autoupdate if GTAGS size is larger than 50MB. */
-                const size = this.global.getGtagsSize(docChanged);
-                if (size >= 50*1024*1024)
-                    return;
-            }
-            this.global.updateTags(docChanged);
-        } catch (e) {
-            Logger.error("autoUpdateTags failed: " + e);
-        }
-    }
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,17 @@
-'use strict';
-import * as vscode from 'vscode';
-import Logger from './logger';
-import Global from './global/global';
-import Gtags from './global/gtags';
-import Configuration from './configuration';
-import AutoUpdateHandler from './autoUpdateHandler';
-import ShowVersionHandler from './showVersionHandler';
-import RebuildGtagsHandler from './rebuildGtagsHandler';
-import DefinitionProvider from './definitionProvider';
-import ReferenceProvider from './referenceProvider';
-import CompletionItemProvider from './completionItemProvider';
-import DocumentSymbolProvider from './documentSymbolProvider';
-import WorkspaceSymbolProvider from './workspaceSymbolProvider';
+"use strict";
+import * as vscode from "vscode";
+import Logger from "./logger";
+import Global from "./global/global";
+import Gtags from "./global/gtags";
+import Configuration from "./configuration";
+import AutoUpdateHandler from "./autoUpdateHandler";
+import ShowVersionHandler from "./showVersionHandler";
+import RebuildGtagsHandler from "./rebuildGtagsHandler";
+import DefinitionProvider from "./definitionProvider";
+import ReferenceProvider from "./referenceProvider";
+import CompletionItemProvider from "./completionItemProvider";
+import DocumentSymbolProvider from "./documentSymbolProvider";
+import WorkspaceSymbolProvider from "./workspaceSymbolProvider";
 
 const configuration = new Configuration();
 
@@ -22,36 +22,71 @@ const autoUpdateHandler = new AutoUpdateHandler(global, configuration);
 const showVersionHandler = new ShowVersionHandler(global);
 const rebuildGtagsHandler = new RebuildGtagsHandler(gtags);
 
-const disposables:vscode.Disposable[] = [];
+const disposables: vscode.Disposable[] = [];
+
+const supportLanguages = ["cpp", "c", "php", "javascript", "javascriptreact"];
 
 // Called when the extension is activated
 // The extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  Logger.init(configuration);
+  Logger.info("Init logger successfully");
 
-    Logger.init(configuration);
-    Logger.info("Init logger successfully");
+  disposables.push(
+    vscode.languages.registerDefinitionProvider(
+      supportLanguages,
+      new DefinitionProvider(global)
+    )
+  );
+  disposables.push(
+    vscode.languages.registerReferenceProvider(
+      supportLanguages,
+      new ReferenceProvider(global)
+    )
+  );
+  disposables.push(
+    vscode.languages.registerCompletionItemProvider(
+      supportLanguages,
+      new CompletionItemProvider(global, configuration)
+    )
+  );
+  disposables.push(
+    vscode.languages.registerDocumentSymbolProvider(
+      supportLanguages,
+      new DocumentSymbolProvider(global)
+    )
+  );
+  disposables.push(
+    vscode.languages.registerWorkspaceSymbolProvider(
+      new WorkspaceSymbolProvider(global)
+    )
+  );
 
-    disposables.push(vscode.languages.registerDefinitionProvider(['cpp', 'c'],
-                     new DefinitionProvider(global)));
-    disposables.push(vscode.languages.registerReferenceProvider(['cpp', 'c'],
-                     new ReferenceProvider(global)));
-    disposables.push(vscode.languages.registerCompletionItemProvider(['cpp', 'c'],
-                     new CompletionItemProvider(global, configuration)));
-    disposables.push(vscode.languages.registerDocumentSymbolProvider(['cpp', 'c'],
-                     new DocumentSymbolProvider(global)));
-    disposables.push(vscode.languages.registerWorkspaceSymbolProvider(new WorkspaceSymbolProvider(global)));
+  disposables.push(
+    vscode.commands.registerCommand(
+      "extension.showGlobalVersion",
+      showVersionHandler.showGlobalVersion,
+      showVersionHandler
+    )
+  );
+  disposables.push(
+    vscode.commands.registerCommand(
+      "extension.rebuildGtags",
+      rebuildGtagsHandler.rebuildGtags,
+      rebuildGtagsHandler
+    )
+  );
+  disposables.push(
+    vscode.workspace.onDidSaveTextDocument(
+      (doc) => autoUpdateHandler.autoUpdateTags(doc),
+      autoUpdateHandler
+    )
+  );
 
-    disposables.push(vscode.commands.registerCommand('extension.showGlobalVersion',
-                     showVersionHandler.showGlobalVersion, showVersionHandler));
-    disposables.push(vscode.commands.registerCommand('extension.rebuildGtags',
-                     rebuildGtagsHandler.rebuildGtags, rebuildGtagsHandler));
-    disposables.push(vscode.workspace.onDidSaveTextDocument(
-                     doc => autoUpdateHandler.autoUpdateTags(doc), autoUpdateHandler));
-
-    Logger.info("Extension is now active!");
+  Logger.info("Extension is now active!");
 }
 
 // This method is called when the extension is deactivated
 export function deactivate() {
-    disposables.forEach(d => d.dispose());
+  disposables.forEach((d) => d.dispose());
 }
